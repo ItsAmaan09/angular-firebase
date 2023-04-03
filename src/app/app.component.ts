a1350452262b63daefae033173c885d3822f7327
Mohammed Amaan <mohammed.a@wiseapp.ai>
2023-04-03 16:42:48 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `<router-outlet></router-outlet>
-    <app-toastr></app-toastr>`,
+    <app-toastr></app-toastr> 
+    `,
   styleUrls: []
 })
 export class AppComponent {
